02242cc1c61c7143723abc6ac21703bc8f18b16f
assafchamoy <assafchamoy97@gmail.com>
2022-03-15 15:51:32 +0200

    fix: multi namespaced  services to single

--- src/getServicesFromK8s/controllers/getServicesFromK8sController.ts
+++ src/getServicesFromK8s/controllers/getServicesFromK8sController.ts
@@ -11,21 +11,43 @@ type GetServicesFromCluster = RequestHandler<undefined>;
 
 @injectable()
 export default class GetServicesFromK8sController {
+<<<<<<< HEAD
   private readonly namespace: string;
+=======
+  private readonly namespaces: string[] = [];
+>>>>>>> cdf126ca071629292c2ded7f85624959d5eba732
 
   public constructor(
     @inject(SERVICES.LOGGER) private readonly logger: Logger,
     @inject(SERVICES.K8S_OPERATIONS) private readonly k8s: K8sOperations,
     @inject(SERVICES.CONFIG) private readonly config: IConfig
   ) {
+<<<<<<< HEAD
     this.namespace = this.config.get<string>('kubernetes.namespace');
+=======
+    this.namespaces = this.config.get<string[]>('kubernetes.namespaces');
+>>>>>>> cdf126ca071629292c2ded7f85624959d5eba732
   }
 
   public getDeploymentsAndServicesFromCluster: GetServicesFromCluster = async (req, res, next) => {
     try {
+<<<<<<< HEAD
       const depsAndSvcs = await this.k8s.getDeploymentsAndServices(this.namespace);    
 
       res.send(depsAndSvcs);
+=======
+      const namespacedDepsAndSvcs = this.namespaces.reduce((acc: Record<string, DepsAndServices[]>, ns) => {
+        acc[ns] = [];
+        return acc;
+      }, {});
+
+      for(const ns of Object.keys(namespacedDepsAndSvcs)){
+        const depsAndSvcs = await this.k8s.getDeploymentsAndServices(ns);
+        namespacedDepsAndSvcs[ns] = depsAndSvcs;
+      }
+
+      res.send(namespacedDepsAndSvcs);
+>>>>>>> cdf126ca071629292c2ded7f85624959d5eba732
     } catch (e) {
       next(e);
     }
