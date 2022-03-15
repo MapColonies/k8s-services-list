c7ec2ea25ee43c93cb55c3991189dc7220e5f819
assafchamoy <assafchamoy97@gmail.com>
2022-03-15 15:48:45 +0200

    fix: multi namespaced  services to single

--- src/getServicesFromK8s/controllers/getServicesFromK8sController.ts
+++ src/getServicesFromK8s/controllers/getServicesFromK8sController.ts
@@ -1,24 +1,31 @@
 import { Logger } from '@map-colonies/js-logger';
+import { IConfig } from 'config';
 import { RequestHandler } from 'express';
 import httpStatus from 'http-status-codes';
 import { injectable, inject } from 'tsyringe';
 import { SERVICES } from '../../common/constants';
+import { DepsAndServices } from '../../common/interfaces';
 import K8sOperations from '../../common/utils/k8sOperations';
 
 type GetServicesFromCluster = RequestHandler<undefined>;
 
 @injectable()
 export default class GetServicesFromK8sController {
+  private readonly namespace: string;
+
   public constructor(
     @inject(SERVICES.LOGGER) private readonly logger: Logger,
-    @inject(SERVICES.K8S_OPERATIONS) private readonly k8s: K8sOperations
-  ) {}
+    @inject(SERVICES.K8S_OPERATIONS) private readonly k8s: K8sOperations,
+    @inject(SERVICES.CONFIG) private readonly config: IConfig
+  ) {
+    this.namespace = this.config.get<string>('kubernetes.namespace');
+  }
 
   public getDeploymentsAndServicesFromCluster: GetServicesFromCluster = async (req, res, next) => {
     try {
-        const deploymentsAndServices = await this.k8s.getDeploymentsAndServices();
-        
-        res.send(deploymentsAndServices);
+      const depsAndSvcs = await this.k8s.getDeploymentsAndServices(this.namespace);    
+
+      res.send(depsAndSvcs);
     } catch (e) {
       next(e);
     }
